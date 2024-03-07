895b2987df2a5f2bc9de510441f1dd003e3bb65e
Egor <68235201+Intellectual23@users.noreply.github.com>
2024-03-08 02:37:17 +0300

    feat(code): add CardGame
    1)Add SingleTone CadGame, with Dictionary<CardInstance, Cardview> and List<CardAssets>
    2) Add StartGame() for 1 player

---
+++ CardGame/Assets/_Source/CardGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Card;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+  public class CardGame : MonoBehaviour
+  {
+    [SerializeField] private GameObject _cardPrefab;
+
+    public static CardGame Instance;
+    public List<CardAsset> _startCards = new();
+    private Dictionary<CardInstance, CardView> _cards = new();
+
+    private void Awake()
+    {
+      if (Instance == null)
+      {
+        Instance = this;
+      }
+      else
+      {
+        Destroy(gameObject);
+      }
+
+      DontDestroyOnLoad(this);
+    }
+
+    private void StartGame()
+    {
+      for (int i = 0; i < _startCards.Count; ++i)
+      {
+        CardInstance cardInstance = new CardInstance(_startCards[i]);
+        GameObject cardObject = Instantiate(_cardPrefab);
+        CardView cardView = _cardPrefab.AddComponent<CardView>();
+        cardView.Init(new CardInstance(_startCards[i]));
+        _cards.Add(cardInstance, cardView);
+      }
+    }
+  }
+}
