c9ced0a0449dabfa1684226195d79536bc6a75e3
Egor <68235201+Intellectual23@users.noreply.github.com>
2024-03-07 23:08:37 +0300

    feat(code): add CardAsset, CardInstance  and CardView

---
+++ CardGame/Assets/_Source/Card/CardInstance.cs
@@ -0,0 +1,12 @@
+﻿namespace Card
+{
+  public class CardInstance
+  {
+    private CardAsset _cardAsset;
+
+    public CardInstance(CardAsset cardAsset)
+    {
+      _cardAsset = cardAsset;
+    }
+  }
+}
---
+++ CardGame/Assets/_Source/Card/CardView.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Card
+{
+  public class CardView: MonoBehaviour
+  {
+    private CardInstance _cardInstance;
+
+    public void Init(CardInstance cardInstance)
+    {
+      _cardInstance = cardInstance;
+    }
+  }
+}
