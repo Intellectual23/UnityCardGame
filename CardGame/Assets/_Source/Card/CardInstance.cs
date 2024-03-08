49ab8541bd61a99b6d04d7b1efe59461bc5e07ca
Egor <68235201+Intellectual23@users.noreply.github.com>
2024-03-08 04:05:46 +0300

    fix(code): divided StartGame into CreateCardView aand CreateCard, add MoveToLayout into CardInstance
    1) StartGame -> for with CreateCard -> CreateCardView
    2) Updated CardInstance, add layoutId, cardPosition and MoveToLayout method.

--- CardGame/Assets/_Source/Card/CardInstance.cs
+++ CardGame/Assets/_Source/Card/CardInstance.cs
@@ -3,10 +3,17 @@
   public class CardInstance
   {
     private CardAsset _cardAsset;
-
+    public int LayoutId;
+    public int CardPosition;
     public CardInstance(CardAsset cardAsset)
     {
       _cardAsset = cardAsset;
+    }
+
+    public void MoveToLayout(int newLayoutId, int newCardPosition)
+    {
+      LayoutId = newLayoutId;
+      CardPosition = newCardPosition;
     }
   }
 }
--- CardGame/Assets/_Source/CardGame.cs
+++ CardGame/Assets/_Source/CardGame.cs
@@ -1,41 +1,52 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Card;
 using UnityEngine;
 
-namespace DefaultNamespace
+public class CardGame : MonoBehaviour
 {
-  public class CardGame : MonoBehaviour
+  [SerializeField] private GameObject _cardPrefab;
+
+  public static CardGame Instance;
+  public List<CardAsset> _startCards = new();
+  private Dictionary<CardInstance, CardView> _cards = new();
+
+  private void Awake()
   {
-    [SerializeField] private GameObject _cardPrefab;
-
-    public static CardGame Instance;
-    public List<CardAsset> _startCards = new();
-    private Dictionary<CardInstance, CardView> _cards = new();
-
-    private void Awake()
+    if (Instance == null)
     {
-      if (Instance == null)
-      {
-        Instance = this;
-      }
-      else
-      {
-        Destroy(gameObject);
-      }
-
-      DontDestroyOnLoad(this);
+      Instance = this;
+    }
+    else
+    {
+      Destroy(gameObject);
     }
 
-    private void StartGame()
+    DontDestroyOnLoad(this);
+  }
+
+  private void StartGame()
+  {
+    for (int i = 0; i < _startCards.Count; ++i)
     {
-      for (int i = 0; i < _startCards.Count; ++i)
-      {
-        CardInstance cardInstance = new CardInstance(_startCards[i]);
-        GameObject cardObject = Instantiate(_cardPrefab);
-        CardView cardView = _cardPrefab.AddComponent<CardView>();
-        cardView.Init(new CardInstance(_startCards[i]));
-        _cards.Add(cardInstance, cardView);
-      }
+      CreateCard(_startCards[i], 0);
     }
+  }
+
+  private void CreateCardView(CardInstance cardInstance)
+  {
+    GameObject cardObject = Instantiate(_cardPrefab);
+    CardView cardView = _cardPrefab.AddComponent<CardView>();
+    cardView.Init(cardInstance);
+    _cards.Add(cardInstance, cardView);
+  }
+
+  private void CreateCard(CardAsset cardAsset, int layoutId)
+  {
+    CardInstance cardInstance = new CardInstance(cardAsset);
+    CreateCardView(cardInstance);
+    // Moving card to Layout. Use LINQ to get position(starts from 1).
+    var layoutCards = _cards.Where(pair => pair.Key.LayoutId == layoutId).Select(pair => pair.Value).ToList();
+    cardInstance.MoveToLayout(layoutId, layoutCards.Count + 1);
   }
 }
